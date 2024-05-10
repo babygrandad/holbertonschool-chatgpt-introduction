41ae53aeca120b398ac944e830cdbaed20bfd7a8
babygrandad <relebogilenkotswe@gmail.com>
2024-05-10 14:42:44 +0200

    fixed file types

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,23 +2,11 @@
 import sys
 
 def factorial(n):
-    if n < 0:
-        return "Undefined for negative numbers"
     result = 1
     while n > 1:
         result *= n
-        n -= 1
+        n = n - 1
     return result
 
-if len(sys.argv) < 2:
-    print("Usage: python3 script.py <number>")
-    sys.exit(1)
-
-try:
-    number = int(sys.argv[1])
-except ValueError:
-    print("Please enter a valid integer.")
-    sys.exit(1)
-
-f = factorial(number)
+f = factorial(int(sys.argv[1]))
 print(f)
